41a23278f38b452dfd57a345bea7781403bd3d0b
Liuqh <528851597@qq.com>
2021-02-26 10:50:25 +0800

    finish menus

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,34 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // 当插件被激活的时候会调用这个方法
 export function activate(context: vscode.ExtensionContext) {
-  console.log('插件被激活');
+  console.log("插件被激活");
 
   // 这个命令在package.json文件中已经注册了
   // 我们在这里需要注册一个该命令的监听事件
-  let disposable = vscode.commands.registerCommand('auto-toggle-theme.helloWorld', () => {
-    // 当命令被执行的时候，这里的代码就会被执行
+  let disposable = vscode.commands.registerCommand(
+    "auto-toggle-theme.helloWorld",
+    () => {
+      // 当命令被执行的时候，这里的代码就会被执行
 
-    // 向用户展示一个信息框
-    vscode.window.showInformationMessage('Yep！成功啦!哈哈哈哈哈能监听到修改吗');
-  });
+      // 向用户展示一个信息框
+      vscode.window.showInformationMessage("Yep！成功啦!");
+    }
+  );
 
-  context.subscriptions.push(disposable);
+  let goCommand = vscode.commands.registerCommand(
+    "auto-toggle-theme.go",
+    () => {
+      vscode.window.showWarningMessage("hahahahahaha");
+    }
+  );
+  // 存放注册的命令，这个数组中的事件，在插件被释放时也会被处理
+  context.subscriptions.push(disposable, goCommand);
 }
 
 /**
  * 插件被释放时执行
  */
 export function deactivate() {
-  console.log('插件已经释放');
+  console.log("插件已经释放");
 }
