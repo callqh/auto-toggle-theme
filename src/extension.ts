c40782673b41ee2e900c7779ab8d39219c49082b
Liu <528851597@qq.com>
2021-02-27 21:53:35 +0800

    feat: 完成菜单栏配置

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,8 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
   console.log('插件被激活');
 
-  // 这个命令在package.json文件中已经注册了
   // 我们在这里需要注册一个该命令的监听事件
   let disposable = vscode.commands.registerCommand('auto-toggle-theme.helloWorld', () => {
-    // 当命令被执行的时候，这里的代码就会被执行
-
     // 向用户展示一个信息框
     vscode.window.showInformationMessage('Yep！成功啦!哈哈哈哈哈能监听到修改吗');
   });
